9d31b087dd6fa27fde68792fec79db0db9455797
ShuvamLabs <shuvamchatterjee007@gmail.com>
2026-08-12 03:51:12 +0530

    modified predict.py

--- src/predict.py
+++ src/predict.py
@@ -3,7 +3,7 @@ import pandas as pd
 from pathlib import Path
 
 def loadmodel():
-    path = Path(__file__).resolve().parent.parent/ "model" / "best-model.pkl"
+    path = Path(__file__).resolve().parent.parent / "model" / "best-model.pkl"
     return joblib.load(path)
 
 def predict(new_data):
